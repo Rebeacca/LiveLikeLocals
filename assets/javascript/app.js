b496db935e1213cc74f3ae17a67774c029afeb95
Rebeacca Modayil <rebeacca.modayil@temple.edu>
2019-07-08 19:34:53 -0400

    updated login: can click outside modal to get rid of it

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -270,11 +270,12 @@ function getPlacesPhoto(search) {
     console.log("🏔");
   });
 }
-
+let modal = document.getElementById('id01');
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
+  console.log(1)
 }
 getPlacesPhoto('london');
